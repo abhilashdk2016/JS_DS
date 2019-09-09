89c6189c1404cd5e5dfc4e9ba6418145480ce05f
abhilashdk2016 <abhilashdk2014@outlook.com>
2019-09-09 09:59:30 +0530

    Graph

--- src/data-structures/graph.ts
+++ src/data-structures/graph.ts
@@ -1,3 +1,51 @@
-export default class Graph<T> {
-    
+import Dictionary from './dictionary';
+export default class Graph {
+    private vertices: (string | number)[] = [];
+    private adjList: Dictionary<string | number, (string | number)[]> = new Dictionary();
+
+    constructor(private isDirected = false) {}
+
+    addVertex(v: string | number) {
+        if (!this.vertices.includes(v)) {
+            this.vertices.push(v);
+            this.adjList.set(v, []);
+        }
+    }
+
+    addEdge(a: string | number, b: string | number) {
+        if (!this.adjList.get(a)) {
+            this.addVertex(a);
+        }
+
+        if (!this.adjList.get(b)) {
+            this.addVertex(b);
+        }
+
+        this.adjList.get(a).push(b);
+
+        if (!this.isDirected) {
+            this.adjList.get(b).push(a);
+        }
+    }
+
+    getVertices() {
+        return this.vertices;
+    }
+
+    getAdjList() {
+        return this.adjList;
+    }
+
+    toString() {
+        let s = '';
+        for (let i = 0; i < this.vertices.length; i++) {
+            s += this.vertices[i] + ' -> ';
+            const neighbors = this.adjList.get(this.vertices[i]);
+            for (let j = 0; j < neighbors.length; j++) {
+            s += neighbors[j] + ' ';
+            }
+            s += '\n';
+        }
+        return s;
+    }
 }
